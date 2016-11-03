2744754844d15862b7f4047461f38c7493b2487d
snazzythat <roman.andoni@gmail.com>
2016-11-03 00:25:14 -0400

    Holy fuck, finished the update logic. Now corners, edges and middle is updating. Update happens if a process gets one dot or a process gets many dots. Now the only thing left is the MPI shit

--- grid4_4.py
+++ grid4_4.py
@@ -46,9 +46,9 @@ def fill_node_dict(dot_amount_per_process, rank):
                     rank_counter += 1
                     if rank_counter == rank:
                         if (i == GRID_SIZE/2) and (j == GRID_SIZE/2):
-                            node_dict[(i,j)] = [float(0), float(0), float(1)]
+                            node_dict[(i,j)] = [0.0, 0.0, 0.0]
                         else:
-                            node_dict[(i,j)] = [float(0), float(0), float(0)]
+                            node_dict[(i,j)] = [0.0, 0.0, 0.0]
     #case N=512
     else:
         offset = GRID_SIZE/size
@@ -57,9 +57,9 @@ def fill_node_dict(dot_amount_per_process, rank):
         for j in range(beginning, end):
             for i in range(0, GRID_SIZE):
                 if (i == GRID_SIZE / 2) and (j == GRID_SIZE / 2):
-                    node_dict[(i, j)] = [float(1) float(0), float(0)]
+                    node_dict[(i, j)] = [0.0, 0.0, 0.0]
                 else:
-                    node_dict[(i, j)] = [float(0), float(0), float(0)]
+                    node_dict[(i, j)] = [0.0, 0.0, 0.0]
 
 
 
@@ -85,6 +85,9 @@ def simulate_iteration(condition = None):
     #So we check the key first, if its in processes pool of keys.
     #If the key is in the pool, check the received neighbor values.
     #Take the value (t2, t1 or t0) basing on the case (corner, middle or edges)
+    # ex neighbor_nodes_and_their_values[(1,0)][2] means we get the most recent value (position 2 in value array)
+    #  of  neighbor that is situated at 1,0
+
     key_list = node_dict.keys()
 
     neigbour_key_list = neigbour_nodes_and_their_values.keys()
@@ -93,25 +96,25 @@ def simulate_iteration(condition = None):
         #case upper left corner. depends on previous value of node i=1,j=0
         if (0,0) in key_list:
             if ((1,0) in neigbour_key_list):
-                update_value = G * neigbour_key_list[(1,0)][2]
+                update_value = G * neigbour_nodes_and_their_values[(1,0)][2]
                 node_dict[(0,0)][2] = update_value
 
         # case upper right corner. depends on previous value of node i=N-2,j=0
         elif (GRID_SIZE-1, 0) in key_list:
             if ((GRID_SIZE-2, 0) in neigbour_key_list):
-                update_value = G * neigbour_key_list[(GRID_SIZE-2, 0)][2]
+                update_value = G * neigbour_nodes_and_their_values[(GRID_SIZE-2, 0)][2]
                 node_dict[(GRID_SIZE-1, 0)][2] = update_value
 
         # case lower  left corner. depends on previous value of node i=0,j=N-2
         elif (0, GRID_SIZE-1) in key_list:
             if ((0, GRID_SIZE-2) in neigbour_key_list):
-                update_value = G * neigbour_key_list[(0, GRID_SIZE-2)][2]
+                update_value = G * neigbour_nodes_and_their_values[(0, GRID_SIZE-2)][2]
                 node_dict[(0,GRID_SIZE-1)][2] = update_value
 
         # case lower right corner. depends on previous value of node i=N-1,j=N-2
         elif (GRID_SIZE-1, GRID_SIZE-1) in key_list:
             if ((GRID_SIZE-1, GRID_SIZE-2) in neigbour_key_list):
-                update_value = G * neigbour_key_list[(GRID_SIZE-1, GRID_SIZE-2)][2]
+                update_value = G * neigbour_nodes_and_their_values[(GRID_SIZE-1, GRID_SIZE-2)][2]
                 node_dict[(GRID_SIZE-1,GRID_SIZE-1)][2] = update_value
 
 
@@ -120,33 +123,58 @@ def simulate_iteration(condition = None):
         for j in range(1,GRID_SIZE-1):
             if (0,j) in key_list:
                 if(1,j) in neigbour_key_list:
-                    update_value = G * neigbour_key_list[(1,j)][2]
+                    update_value = G * neigbour_nodes_and_their_values[(1,j)][2]
                     node_dict[(0,j)][2] = update_value
 
         #right  most edge case --> get all nodes with i at N-1 j starting from 1 till N-2
         for j in range(1,GRID_SIZE-1):
             if (GRID_SIZE-1,j) in key_list:
                 if (GRID_SIZE-2,j) in neigbour_key_list:
-                    update_value = G * neigbour_key_list[(GRID_SIZE-2,j)][2]
+                    update_value = G * neigbour_nodes_and_their_values[(GRID_SIZE-2,j)][2]
                     node_dict[(GRID_SIZE-1,j)][2] = update_value
 
         #right top edge case --> get all nodes with j at 0 and x starting from 1 till N-2
         for i in range(1,GRID_SIZE-1):
             if (i,0) in key_list:
                 if (i,1) in neigbour_key_list:
-                    update_value = G * neigbour_key_list[(i,1)][2]
+                    update_value = G * neigbour_nodes_and_their_values[(i,1)][2]
                     node_dict[(i,0)][2] = update_value
 
         #bottom top edge case --> get all nodes with j at 0 and x starting from 1 till N-2
         for i in range(1,GRID_SIZE-1):
             if (i,GRID_SIZE-1) in key_list:
                 if (i,GRID_SIZE-2) in neigbour_key_list:
-                    update_value = G * neigbour_key_list[(i,GRID_SIZE-2)][2]
+                    update_value = G * neigbour_nodes_and_their_values[(i,GRID_SIZE-2)][2]
                     node_dict[(i,GRID_SIZE-1)][2] = update_value
 
     elif(condition == "CENTER"):
         #CENTER CASE
-        #All (i,j) pairs are
+        #All i's are situated between 1 and N-2
+        #All j's are situated between 1 and N-2
+        #Get all 4 neigbours values and then update the value using the formula
+        for i in range(1,GRID_SIZE-1):
+            for j in range(1,GRID_SIZE-1):
+                if(i,j) in key_list:
+
+                    left_neigbour_prev_val = 0
+                    right_neighbour_prev_val = 0
+                    lower_neighbour_prev_val = 0
+                    upper_neighbour_prev_val = 0
+                    #now treat every neighbour separately
+                    if(i-1,j) in neigbour_key_list:
+                        left_neigbour_prev_val = neigbour_nodes_and_their_values[(i-1,j)][1]
+                    if(i+1,j) in neigbour_key_list:
+                        right_neighbour_prev_val = neigbour_nodes_and_their_values[(i+1,j)][1]
+                    if(i, j-1) in neigbour_key_list:
+                        lower_neighbour_prev_val = neigbour_nodes_and_their_values[(i, j-1)][1]
+                    if(i, j+1) in neigbour_key_list:
+                        upper_neighbour_prev_val = neigbour_nodes_and_their_values[(i, j+1)][1]
+
+                    glorious_center_value = (RHO * (left_neigbour_prev_val + right_neighbour_prev_val + \
+                            upper_neighbour_prev_val + lower_neighbour_prev_val - (4*node_dict[(i,j)][1])) + \
+                            2*(node_dict[(i,j)][1]) - (1 - ETA)*(node_dict[(i,j)][0]))/(1-ETA)
+
+
 
 if __name__ == "__main__":
 
@@ -164,7 +192,6 @@ if __name__ == "__main__":
     #for t-2,t-1,t values
     fill_node_dict(dots_per_process,rank)
 
-     # print "I'm rank " + str(rank) + " and my grid dict is" + str(node_dict) + '\n\n'
 
     # For each of iteration, do 3 updates. First center, then edges, then corners.
     # Then print the value at N/2 N/2 where strike was applied
@@ -176,6 +203,8 @@ if __name__ == "__main__":
 
         simulate_iteration(condition = "CORNER")
 
+        print "I'm rank " + str(rank) + " and my grid dict is" + str(node_dict) + '\n\n'
+
         #STRIKE PRINT
         key_list = node_dict.keys()
         if((GRID_SIZE/2,GRID_SIZE/2) in key_list):
@@ -186,8 +215,6 @@ if __name__ == "__main__":
 
         iterations -= 1
 
-
+        #at each iteration also shift previous values.
     MPI.Finalize()
-
-    print 'END'
 
